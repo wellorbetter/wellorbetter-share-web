b1d6dd71918ec602d0af00745a6d995509bedc0c
wellorbetter <1419919418@qq.com>
2026-08-28 23:41:47 +0800

    feat(portfolio): route generic developer profiles

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { themeStyle } from "@wellorbetter/design";
-import App from "./App.js";
+import AppService from "./AppService.js";
+import PortfolioPage from "./PortfolioPage.js";
 import "./styles.css";
 
 const styleEl = document.createElement("style");
 styleEl.textContent = themeStyle;
 document.head.appendChild(styleEl);
 
+const match = window.location.pathname.match(/^\/u\/([^/]+)\/?$/);
+const content = match ? <PortfolioPage username={decodeURIComponent(match[1]!)} /> : <AppService />;
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {content}
   </React.StrictMode>,
 );
